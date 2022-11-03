cf151074bdafcab3d9d81d2788e0b489c5fe10ee
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-11-03 09:25:59 -0300

    Occupation

---
+++ src/domains/EducationLevel.go
@@ -0,0 +1,17 @@
+package domains
+
+type EducationLevel struct {
+	nome string
+}
+
+
+const (
+	NAO_ALFABETIZADO = EducationLevel{"Não alfabetizado"}
+	ENSINO_FUNDAMENTAL_INCOMPLETO = EducationLevel{"Ensino Fundamental incompleto"}
+	ENSINO_FUNDAMENTAL_COMPLETO = EducationLevel{"Ensino Fundamental completo"}
+	ENSINO_MEDIO_INCOMPLETO = EducationLevel{"Ensino Médio incompleto"}
+	ENSINO_MEDIO_COMPLETO = EducationLevel{"Ensino Médio completo"}
+	ENSINO_SUPERIOR_INCOMPLETO = EducationLevel{"Ensino Superior incompleto"}
+	ENSINO_SUPERIOR_COMPLETO = EducationLevel{"Ensino Superior completo"}
+	POS_GRADUACAO = EducationLevel{"Pós-graduação"}
+)
