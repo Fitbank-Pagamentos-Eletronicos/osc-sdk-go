8bd0cc8288fd693eab6291d7aa2bde3cc4a62609
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-11-10 10:20:45 -0300

    Refactor

--- src/domains/PipelineMatchLoan.go
+++ src/domains/PipelineMatchLoan.go
@@ -1,11 +1,11 @@
 package domains
 
 type PipelineMatchLoan struct {
-	Id string
-	Status string
-	Cpf string
-	Name string
+	Id          string
+	Status      string
+	Cpf         string
+	Name        string
 	DateCreated string
 	LastUpdated string
-	Matches Match
+	Matches     Match
 }
--- src/domains/PipelineProposal.go
+++ src/domains/PipelineProposal.go
@@ -1,11 +1,10 @@
 package domains
 
 type PipelineProposal struct {
-	id string
-	status string
-	cpf string
-	name string
-	dateCreated string
-	lastUpdated string
+	Id          string
+	Status      string
+	Cpf         string
+	Name        string
+	DateCreated string
+	LastUpdated string
 }
-
