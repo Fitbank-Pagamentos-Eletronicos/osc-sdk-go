d5886ab286ffab7ab8832cb85c70ea16525dd3cb
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-11-10 10:21:38 -0300

    Test

--- test/PipelineMatchLoan_test.go
+++ test/PipelineMatchLoan_test.go
@@ -1,9 +1,74 @@
 package test
 
 import (
+	"modulo/src/domains"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPipelineMatchLoan(t *testing.T) {
-	// Em progresso ...
+
+	matchLoan := domains.MatchLoan{
+		ProductId:      1,
+		Name:           "Teste",
+		Logo:           "Teste",
+		MinValue:       1,
+		MaxValue:       1000,
+		MinInstallment: 1,
+		MaxInstallment: 12,
+		MonthlyTax:     1.0,
+	}
+
+	matchCard := domains.MatchCard{
+		ProductId: 2,
+		Name:      "Teste",
+		Logo:      "Teste",
+		Annuity:   1.0,
+		Network:   domains.VISA,
+	}
+	matchAuto := domains.MatchAuto{
+		ProductId:      3,
+		Name:           "Teste",
+		Logo:           "Teste",
+		MinValue:       1,
+		MaxValue:       1000,
+		MinInstallment: 1,
+		MaxInstallment: 12,
+		MonthlyTax:     2.0,
+	}
+
+	matchHome := domains.MatchHome{
+		ProductId:      5,
+		Name:           "Teste",
+		Logo:           "Teste",
+		MinValue:       1,
+		MaxValue:       1000,
+		MinInstallment: 1,
+		MaxInstallment: 16,
+		MonthlyTax:     1.0,
+	}
+	matchs := domains.Match{
+		MatchLoan: matchLoan,
+		MatchCard: matchCard,
+		MatchAuto: matchAuto,
+		MatchHome: matchHome,
+	}
+
+	pipelineMatchLoan := domains.PipelineMatchLoan{
+		Id:          "1",
+		Status:      "Aprovado",
+		Cpf:         "12345678901",
+		Name:        "Emanuel da Silva",
+		DateCreated: "2020-01-01",
+		LastUpdated: "2020-01-01",
+		Matches:     matchs,
+	}
+	assert.Equal(t, "1", pipelineMatchLoan.Id)
+	assert.Equal(t, "Aprovado", pipelineMatchLoan.Status)
+	assert.Equal(t, "12345678901", pipelineMatchLoan.Cpf)
+	assert.Equal(t, "Emanuel da Silva", pipelineMatchLoan.Name)
+	assert.Equal(t, "2020-01-01", pipelineMatchLoan.DateCreated)
+	assert.Equal(t, "2020-01-01", pipelineMatchLoan.LastUpdated)
+	assert.Equal(t, matchs, pipelineMatchLoan.Matches)
 }
