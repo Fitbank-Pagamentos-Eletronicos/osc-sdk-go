e7a1927b4afddbabb9b8527778a90293ab30db49
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-11-11 12:27:38 -0300

    Refactor

--- src/domains/Auto.go
+++ src/domains/Auto.go
@@ -1,25 +1,31 @@
 package domains
 
+import "encoding/json"
+
 type Auto struct {
 	CustomerServiceNumber string
-	Tipo ProductType
-	Product string
-	ProductId int
-	HasDocuments bool
-	HasContracts bool
-	Logo string
-	LastStatus CreditStatus
-	PendentDocuments PendentDocuments
-	DateCreated string
-	LastUpdated string
-	Value float64
-	Installments int
-	MonthlyTax float64
-	InstallmentsValue float64
-	IofValeu float64
-	GrossValeu	float64
-	FirstPaymentDate string
-	Cet float64
-	ReleaseDate string
+	Tipo                  ProductType
+	Product               string
+	ProductId             int
+	HasDocuments          bool
+	HasContracts          bool
+	Logo                  string
+	LastStatus            CreditStatus
+	PendentDocuments      PendentDocuments
+	DateCreated           string
+	LastUpdated           string
+	Value                 float64
+	Installments          int
+	MonthlyTax            float64
+	InstallmentsValue     float64
+	IofValeu              float64
+	GrossValeu            float64
+	FirstPaymentDate      string
+	Cet                   float64
+	ReleaseDate           string
 }
 
+func (a *Auto) ToJson() string {
+	json, _ := json.Marshal(a)
+	return string(json)
+}
