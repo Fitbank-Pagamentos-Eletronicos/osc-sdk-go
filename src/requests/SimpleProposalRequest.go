a72651bb324ed9a22cc37d9cd634cdf8a2caeac6
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-12-08 16:17:18 -0300

    feature: Requisição SimpleProposalRequest

--- src/requests/SimpleProposalRequest.go
+++ src/requests/SimpleProposalRequest.go
@@ -11,7 +11,29 @@ import (
 
 )
 
-var dataSimpleProposal = domains.Proposals {
+var dataSimpleProposal = domains.ProposalBankAccount {
+    Mother:     "Fulana Mãe",
+    Gender: domains.FEMININO,
+    Nationality: domains.BRASILEIRA,
+    HomeTownState: domains.GOIAS,
+    RelationshipStatus: domains.CASADO,
+
+    Address: domains.Address{
+        ZipCode: "74000000",
+        Address: "Cidade de Goiânia",
+        Number: "0",
+        Complement: "Casa 1",
+        District: "Geral",
+        State: domains.GOIAS,
+        City: "Goiânia",
+    }
+    Business: domains.Business{
+        Income: "1000.00",
+    }
+
+    Products: domains.Products{
+
+    }
 
 
 }
