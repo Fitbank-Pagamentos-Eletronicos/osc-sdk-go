9a6f0dd7beeb4cfb47b52f67053df12fbb10741d
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-12-08 14:18:10 -0300

    feature: Criação Estrutura SignupMatchResponse

---
+++ src/domains/SignupMatchResponse.go
@@ -0,0 +1,12 @@
+package domains
+
+type SignupMatchResponse struct {
+        ID string `json:"id"`
+        Name string `json:"name"`
+        Status string `json:"status"`
+        Cpf string `json:"cpf"`
+        dataCreated string `json:"dataCreated"`
+        lastUpdate string `json:"lastUpdate"`
+}
+
+
