10f7524282c45d17eca068c8626c4ffc524ef92c
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-12-08 15:44:47 -0300

    feature: Requisição SimplProposalRequest

---
+++ src/requests/SimpleProposalRequest.go
@@ -0,0 +1,17 @@
+package requests
+
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "strings"
+    "modulo/src/domains"
+
+)
+
+var dataSimpleProposal = domains.Proposals {
+
+
+}
