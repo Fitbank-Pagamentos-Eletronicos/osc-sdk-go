b8533f0162fe92e4c242ebfccd41079b5650093f
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-12-08 16:35:26 -0300

    feature: Requisição SimpleProposalRequest

--- src/requests/SimpleProposalRequest.go
+++ src/requests/SimpleProposalRequest.go
@@ -26,14 +26,53 @@ var dataSimpleProposal = domains.ProposalBankAccount {
         District: "Geral",
         State: domains.GOIAS,
         City: "Goiânia",
-    }
+    },
     Business: domains.Business{
         Income: "1000.00",
+    },
+
+
+
+}
+
+func SimpleProposalRequest(ID string) (string, int) {
+    url:= "https://demo-api.easycredito.com.br/api/external//v2.1/process/simple_proposal/" + ID
+    method := "POST"
+
+    fmt.Println("URL:>", url)
+
+    jsonValue, _ := json.Marshal(dataSimpleProposal)
+    payload := strings.NewReader(string(jsonValue))
+
+    client := &http.Client {}
+
+    req, err := http.NewRequest(method, url, payload)
+
+    if err != nil {
+        fmt.Println(err)
+        return "", 0
     }
 
-    Products: domains.Products{
+    req.Header.Add("Content-Type", "application/json")
+    req.Header.Add("Accept", "application/json")
+    req.Header.Add("Authorization", "Bearer " + GetToken())
 
+    res, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+        return "", 0
     }
+
+    defer res.Body.Close()
+
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        fmt.Println(err)
+        return "", 0
+    }
+
+    fmt.Println(string(body))
+    return string(body), res.StatusCode
 
 
 }
