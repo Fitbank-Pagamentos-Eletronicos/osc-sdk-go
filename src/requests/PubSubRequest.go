5ace1fd8f33d27196d32e244e7e89bdde1f1a038
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2022-12-16 10:53:39 -0300

    feature: Criação Requisição PubSub

---
+++ src/requests/PubSubRequest.go
@@ -0,0 +1,45 @@
+package requests
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+)
+
+
+func PubSubRequest() string {
+    url := "https://demo-api.easycredito.com.br/api/external//v2.1/pubsub"
+    method := "GET"
+
+    client := &http.Client {}
+
+    req, err := http.NewRequest(method, url, nil)
+
+    if err != nil {
+        fmt.Println(err)
+        return ""
+    }
+
+    req.Header.Add("Accept", "application/json")
+    req.Header.Add("Authorization", "Bearer " + GetToken())
+
+
+
+    res, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+        return ""
+    }
+
+    defer res.Body.Close()
+
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        fmt.Println(err)
+        return ""
+    }
+    fmt.Println(string(body))
+    return string(body)
+
+
+}
