4f8f5a1aa6bfe31a89be57f6b1bbde72693ed561
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2023-01-12 13:19:55 -0300

    Atualizações de domínios e criação de novos domínios

---
+++ src/domains/EnumEmployeesCount.go
@@ -0,0 +1,11 @@
+package domains
+
+type EmployeeAccount string
+
+const (
+	NENHUM     EmployeeAccount = "NENHUM"
+	DE_1_A_2   EmployeeAccount = "DE_1_A_2"
+	DE_3_A_5   EmployeeAccount = "DE_3_A_5"
+	DE_6_A_10  EmployeeAccount = "DE_6_A_10"
+	MAIS_DE_10 EmployeeAccount = "MAIS_DE_10"
+)
