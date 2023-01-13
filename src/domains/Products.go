ed290d296bcf53171b619fba3b63cfc06a7453a7
Carlos Alexandre Lima da Silva <carlos.lima@fitbank.com.br>
2023-01-13 13:36:04 -0300

    feat: Adicionando na estrutura ProductCaas e ProductWorkingCapital

--- src/domains/Products.go
+++ src/domains/Products.go
@@ -23,6 +23,23 @@ type Products struct {
 	RealEstateType     RealEstateType `json:"realEstateType"`
 	RealEstateValue    float64        `json:"realEstateValue"`
 	OutstandingBalance float64        `json:"outstandingBalance"`
+
+	//Caas
+	ReleaseDate string  `json:"releaseDate"`
+	DueDate     string  `json:"dueDate"`
+	MonthlyTax  float64 `json:"monthlyTax"`
+	CadValue    float64 `json:"cadValue"`
+
+	//WorkingCapital
+	CNPJ                         string                       `json:"cnpj"`
+	BusinessProfession           BusinessProfession           `json:"businessProfession"`
+	EmployeesCount               EmployeesCount               `json:"employeeAccount"`
+	BusinessIncomeCnpj           string                       `json:"businessIncomeCnpj"`
+	WorkingCapitalLoanObjectives WorkingCapitalLoanObjectives `json:"workingCapitalLoanObjectives"`
+	Bank                         Bank                         `json:"bank"`
+	AccountType                  Bank                         `json:"accountType"`
+	Agency                       Bank                         `json:"agency"`
+	Account                      Bank                         `json:"account"`
 }
 
 func (a *Products) ToJson() string {
